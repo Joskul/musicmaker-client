c3eb39bee791015fc52248e5a8662be62685d809
Schpudt <hikaru.kp@gmail.com>
2023-11-29 22:34:01 +0700

    Update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <Layout class="flex flex-col justify-center items-center">
+	<p class="text-9xl my-8 font-signature text-primary">BeatML</p>
 	<p class="text-2xl my-8">Having <i>troubles</i> with your tracks?</p>
 	<a role="button" class="btn" href="/tool">Try this</a>
 </Layout>
