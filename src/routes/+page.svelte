a2e66740643929bf787896fd42d6525ee9f49a30
Joskul <hikaru.kp@gmail.com>
2023-12-05 23:10:39 +0700

    Minor appearance changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <Layout class="flex flex-col justify-center items-center">
-	<p class="text-9xl my-8 font-signature text-primary">BeatML</p>
+	<p class="font-signature text-transparent text-9xl bg-clip-text bg-gradient-to-r from-primary to-accent">BeatML</p>
 	<p class="text-2xl my-8">Having <i>troubles</i> with your tracks?</p>
 	<a role="button" class="btn" href="/tool">Try this</a>
 </Layout>
